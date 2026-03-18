13072a3fc2ae8044397b4bc35bbf73b0a22a6376
Jeycher <jeycherescorcia@gmail.com>
2026-03-18 08:55:29 -0600

    Inicio del poryecto

---
+++ TurismoRural_WEB/TurismoRural_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TurismoRural_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
