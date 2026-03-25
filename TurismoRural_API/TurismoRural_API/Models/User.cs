6a37d7b5457a3bc521865e23cd6b49e4d4496d6d
Brandon Vargas Moreira <136260759+Vargas0421@users.noreply.github.com>
2026-03-25 15:15:14 -0600

    arreglarndo bug del merge

--- TurismoRural_API/TurismoRural_API/Models/User.cs
+++ TurismoRural_API/TurismoRural_API/Models/User.cs
@@ -1,13 +1,14 @@
 namespace TurismoRural_API.Models
 {
+    
     public class User
     {
         public int ID_Usuario { get; set; }
         public string? Nombre { get; set; }
         public string Correo { get; set; } = string.Empty;
         public string? Contrasena { get; set; }
-        public string? Telefono { get; set; }
-        public int ID_Rol { get; set; }
+        public int Telefono { get; set; }
+        public string ID_Rol { get; set; } = "User";
         public DateTime Fecha_Registro { get; set; } = DateTime.UtcNow;
     }
 }
